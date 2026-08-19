e0ae46e19c894807312c172ab900a03ff4d8fd5d
LazyTreeBranch <113071231+lilpat0@users.noreply.github.com>
2026-08-18 22:33:22 -0400

    Update ram8.sv

--- src/ram8.sv
+++ src/ram8.sv
@@ -1,47 +1,28 @@
 module ram8 (
+    input  logic       clk,
+    input  logic       rst_n,
 
-    input logic       clk,
-    input logic       rst_n,
+    input  logic       we,
+    input  logic [3:0] address,
 
-    input logic       we,
-    input logic [4:0] address,
-
-    input logic [7:0] write_data,
+    input  logic [7:0] write_data,
     output logic [7:0] read_data
-
 );
 
-    logic [7:0] memory [0:31];
+    logic [7:0] memory [0:15];
 
-    integer i;
-
-    // Initialize RAM for simulation.
-    initial begin
-        for (i = 0; i < 32; i = i + 1)
-            memory[i] = 8'h00;
-    end
-
-    // Asynchronous read.
+    // Asynchronous read
     always_comb begin
-        read_data = memory[address];
+        if (!rst_n)
+            read_data = 8'h00;
+        else
+            read_data = memory[address];
     end
 
-    // Synchronous write + reset.
-    always_ff @(posedge clk or negedge rst_n) begin
-
-        if (!rst_n) begin
-
-            for (i = 0; i < 32; i = i + 1)
-                memory[i] <= 8'h00;
-
-        end
-
-        else if (we) begin
-
+    // Synchronous write
+    always_ff @(posedge clk) begin
+        if (rst_n && we)
             memory[address] <= write_data;
-
-        end
-
     end
 
 endmodule
